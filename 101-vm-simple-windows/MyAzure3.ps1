e5d8ee403e557e0447af8fc7872cc524547d6f3a
mika <lairimika@gmail.com>
2016-06-07 00:11:18 +0300

    vessio 1.3

--- 101-vm-simple-windows/MyAzure3.ps1
+++ 101-vm-simple-windows/MyAzure3.ps1
@@ -3,6 +3,12 @@ Login-AzureRmAccount
 $rgName1 = "MyDeploymentRG06"
 $LocName1 = “West Europe”
 $templateURI = "https://raw.githubusercontent.com/Lairi/ARM-Templates/master/101-vm-simple-windows/azuredeploy.json"
+echo $rgName + " " +  $LocName
 New-AzureRmResourceGroup -Name $rgName -Location $LocName
-$parameters = @{"newStorageAccountName"="MyDeployStorageACC";"location"= $LocName;”adminUsername”=”Charbel”;”dnsLabelPrefix”=”chmydepl2"}
+$parameters = @{
+                "newStorageAccountName"="MyDeployStorageACC"
+                "location"= $LocName 
+                ”adminUsername”=”Charbel”
+                ”dnsLabelPrefix”=”chmydepl4"
+                }
 New-AzureRmResourceGroupDeployment -Name ExampleDeployment11 -ResourceGroupName $rgName -TemplateUri $templateURI -TemplateParameterObject $parameters
