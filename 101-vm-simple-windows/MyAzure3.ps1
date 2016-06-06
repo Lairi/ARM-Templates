58bdc7be2a79d9ff6687c57aa9f32841d6d3677e
mika <lairimika@gmail.com>
2016-06-06 23:03:12 +0300

    Versio 1.2

--- 101-vm-simple-windows/MyAzure3.ps1
+++ 101-vm-simple-windows/MyAzure3.ps1
@@ -1,8 +1,8 @@
-﻿param([string]$rgName)
+﻿param([string]$rgName,[string]$LocName)
 Login-AzureRmAccount
 $rgName1 = "MyDeploymentRG06"
-$LocName = “West Europe”
+$LocName1 = “West Europe”
 $templateURI = "https://raw.githubusercontent.com/Lairi/ARM-Templates/master/101-vm-simple-windows/azuredeploy.json"
-New-AzureRmResourceGroup -Name $rgName -Location westeurope
-$parameters = @{"newStorageAccountName"="MyDeployStorageACC";"location"="West Europe”;”adminUsername”=”Charbel”;”dnsLabelPrefix”=”chmydepl2"}
+New-AzureRmResourceGroup -Name $rgName -Location $LocName
+$parameters = @{"newStorageAccountName"="MyDeployStorageACC";"location"= $LocName;”adminUsername”=”Charbel”;”dnsLabelPrefix”=”chmydepl2"}
 New-AzureRmResourceGroupDeployment -Name ExampleDeployment11 -ResourceGroupName $rgName -TemplateUri $templateURI -TemplateParameterObject $parameters
